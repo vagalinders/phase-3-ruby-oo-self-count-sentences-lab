d313eaa4e4f7649935e329d1ec6408011fb16722
Chris Mann <chrispmann@gmail.com>
2022-10-12 10:12:01 -0700

    commit tree f885d8e4bbc0dc97dfb9d3f197fbf41cf77e6754

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include? "."
   end
 
   def question?
-
+    self.include? "?"
   end
-
+  
   def exclamation?
-
+    self.include? "!"
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
